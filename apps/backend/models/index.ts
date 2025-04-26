79233ac52338bbb19f9a841206bd98e1bcce480d
sthuray <138075787+sthuray@users.noreply.github.com>
2025-04-25 22:59:34 -0400

    Setup docker

---
+++ apps/backend/models/index.ts
@@ -0,0 +1,14 @@
+import * as path from "path";
+import { Sequelize } from 'sequelize-typescript';
+
+const DATABASE_URI = 
+    process.env.NODE_ENV === "production"
+    ? /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
+      process.env.DATABASE_URI!
+    : `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_DEV}`;
+
+const sequelize = new Sequelize(DATABASE_URI, {
+  models: [path.join(__dirname, "/*.model.ts")],
+});
+
+export { sequelize };
