11216bcb5a53fee776dee36e2553bbcde641d426
sthuray <138075787+sthuray@users.noreply.github.com>
2025-04-28 00:21:01 -0400

    Correct sequelize authentication

--- apps/backend/models/index.ts
+++ apps/backend/models/index.ts
@@ -5,7 +5,7 @@ const DATABASE_URI =
     process.env.NODE_ENV === "production"
     ? /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
       process.env.DATABASE_URI!
-    : `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_DEV}`;
+    : `mysql://${process.env.DB_USER}:${process.env.DB_ROOT_PASSWORD}@${process.env.DB_HOST}:3306`;
 
 const sequelize = new Sequelize(DATABASE_URI, {
   models: [path.join(__dirname, "/*.model.ts")],
