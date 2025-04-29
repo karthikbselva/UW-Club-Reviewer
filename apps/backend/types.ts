1cd21e405676809fde97bed0278e59eabd31cdf8
karthikbselva <karthikbselva@gmail.com>
2025-04-29 14:11:24 -0400

    Added ClubDTO, UserDTO, and enums

--- apps/backend/types.ts
+++ apps/backend/types.ts
@@ -1,4 +1,4 @@
-const clubCategoryValues = [
+const ClubCategoryValues = [
     "Academic",
     "Business and Entrepreneurial",
     "Charitable, Community Service, and International Development",
@@ -11,16 +11,16 @@ const clubCategoryValues = [
     "Political and Social Awareness",
     "Religious and Spiritual",
 ] as const;
-export const clubCategoryEnum: clubCategory[] = [...clubCategoryValues];
-export type clubCategory = typeof clubCategoryValues[number];
+export const ClubCategoryEnum: ClubCategory[] = [...ClubCategoryValues];
+export type ClubCategory = typeof ClubCategoryValues[number];
 
-export enum semesterEnum {
+export enum SemesterEnum {
     FALL = "Fall",
     WINTER = "Winter",
     SPRING = "Spring",
 }
 
-export enum dayEnum {
+export enum DayEnum {
     MON = "Monday",
     TUE = "Tuesday",
     WED = "Wednesday",
@@ -30,12 +30,26 @@ export enum dayEnum {
     SUN = "Sunday",
 }
 
-export type clubDTO = {
+export enum TermEnum {
+   ONE_A  = "1A",
+   ONE_B = "1B",
+   TWO_A = "2A",
+   TWO_B = "2B",
+   THREE_A = "3A",
+   THREE_B = "3B",
+   FOUR_A = "4A",
+   FOUR_B = "4B",
+   FIVE_A = "5A",
+   FIVE_B = "5B",
+}
+
+export type ClubDTO = {
     id: number,
     name: string,
     description: string,
     competitionLevel: number,
     skillLevel: number,
+    /*
     socialMedia: {
         email: string,
         instagram: string,
@@ -44,12 +58,37 @@ export type clubDTO = {
         facebook: string,
         linkedin: string
     },
-    clubCategories: [clubCategory],
+    */
+    clubCategories: [ClubCategory],
     isActive: boolean,
-    activeTerms: [semesterEnum],
-    daysOfOperation: [dayEnum],
+    activeTerms: [SemesterEnum],
+    daysOfOperation: [DayEnum],
     bannerPhoto: string,
 }
-export type createClubDTO = Omit<clubDTO, "id" | "isActive" | "bannerPhoto">;
-export type updateClubDTO = Omit<clubDTO, "id">;
+export type CreateClubDTO = Omit<ClubDTO, "id" | "isActive" | "bannerPhoto">;
+export type UpdateClubDTO = Omit<ClubDTO, "id">;
 
+export type UserDTO = {
+    id: number,
+    email: string,
+    firstName: string,
+    lastName: string,
+    programName: string,
+    termOfStudy: TermEnum,
+    profilePhoto: string,
+}
+
+export type CreateUserDTO = Omit<UserDTO, "id" | "profilePhoto">;
+export type UpdateUserDTO = Omit<UserDTO, "id" | "email">;
+
+export type ReviewDTO = {
+    id: number,
+    userId: number,
+    clubId: number,
+    comment: string,
+    isLiked: boolean,
+    helpfulVotes: number,
+}
+
+export type CreateReviewDTO = Omit<ReviewDTO, "id" | "helpfulVotes">;
+export type UpdateReviewDTO = Omit<ReviewDTO, "id" | "userId" | "clubId" | "helpfulVotes">;
