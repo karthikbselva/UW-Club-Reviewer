fdc3c8d6b96c79df690e41a4d7ece2addd225729
sthuray <138075787+sthuray@users.noreply.github.com>
2025-04-28 20:33:09 -0400

    Correct db URI

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -18,7 +18,7 @@ const DATABASE_URI =
     process.env.NODE_ENV === "production"
     ? /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
       process.env.DATABASE_URI!
-    : `mysql://${process.env.DB_USER}:${process.env.DB_ROOT_PASSWORD}@${process.env.DB_HOST}:3306/uw_club_reviewer_backend`;
+    : `mysql://${process.env.DB_USER}:${process.env.DB_ROOT_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_DEV}`;
 
 const sequelize = new Sequelize(DATABASE_URI, {
   models: [path.join(__dirname, "/*.model.ts")],
