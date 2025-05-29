81260172c235c352ae160ffdec647d0a96c3f826
sthuray <138075787+sthuray@users.noreply.github.com>
2025-05-29 01:17:16 -0400

    Setup routers

--- apps/backend/src/rest/voteRoutes.ts
+++ apps/backend/src/rest/voteRoutes.ts
@@ -5,41 +5,43 @@ import { getErrorMessage } from "../utilities/errorUtils";
 import IReviewService from "../services/interfaces/reviewService";
 import ReviewService from "../services/implementations/reviewService";
 
-const voteRouter : Router = Router();
-const voteService : IVoteService = new VoteService();
+const voteRouter: Router = Router();
+const voteService: IVoteService = new VoteService();
 
-const reviewRouter : Router = Router();
-const reviewService : IReviewService = new ReviewService();
-
+const reviewService: IReviewService = new ReviewService();
 
 voteRouter.post("/votes", async (req, res) => {
-    try {
-        const newVote = await voteService.addVote({
-            userId: req.body.user_id,
-            reviewId: req.body.review_id,
-            voteValue: req.body.vote_value,
-        })
-        // res.status(200).send(newVote);
-        const existingReview = await reviewService.updateReview(req.body.review_id, {
-            comment: null,
-            likesClub: null,
-            voteSum: await voteService.getVoteSum(req.body.review_id),
-        })
-        res.status(200).send(existingReview);
-    } catch (error) {
-        res.status(400).json({ error : getErrorMessage(error) });
-    }
-    
-})
+  try {
+    await voteService.addVote({
+      userId: req.body.user_id,
+      reviewId: req.body.review_id,
+      voteValue: req.body.vote_value,
+    });
+    // res.status(200).send(newVote);
+    const existingReview = await reviewService.updateReview(
+      req.body.review_id,
+      {
+        comment: null,
+        likesClub: null,
+        voteSum: await voteService.getVoteSum(req.body.review_id),
+      },
+    );
+    res.status(200).send(existingReview);
+  } catch (error) {
+    res.status(400).json({ error: getErrorMessage(error) });
+  }
+});
 
 voteRouter.delete("/votes/:reviewId/:userId", async (req, res) => {
-    const userId = parseInt(req.params.userId);
-    const reviewId = parseInt(req.params.reviewId);
+  const userId = parseInt(req.params.userId);
+  const reviewId = parseInt(req.params.reviewId);
 
-    try {
-        await voteService.removeVote(userId, reviewId);
-        res.status(200).send();
-    } catch (error) {
-        res.status(400).json({ error : getErrorMessage(error) });
-    }
-})
+  try {
+    await voteService.removeVote(userId, reviewId);
+    res.status(200).send();
+  } catch (error) {
+    res.status(400).json({ error: getErrorMessage(error) });
+  }
+});
+
+export default voteRouter;
